711687d7dbc02451615bca512ca29886e09c85e4
Saurabh Yadav <ysaurabh@gmail.com>
2020-04-27 23:30:48 +0530

    exercises

--- exercism/java/darts/src/main/java/Darts.java
+++ exercism/java/darts/src/main/java/Darts.java
@@ -1,11 +1,20 @@
 class Darts {
+	private double radius;
 
-    Darts(double x, double y) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	Darts(double x, double y) {
+		radius = Math.sqrt(x * x + y * y);
+	}
 
-    int score() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	int score() {
+		if (radius <= 1) {
+			return 10;
+		} else if (radius <= 5) {
+			return 5;
+		} else if (radius <= 10) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
 
 }
