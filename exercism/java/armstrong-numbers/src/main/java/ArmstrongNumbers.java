adcc0a98e3989b50754b5bb35aa499ecedade56e
Saurabh Yadav <ysaurabh@gmail.com>
2020-04-27 22:55:50 +0530

    armstrong

--- exercism/java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ exercism/java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,26 @@
+import java.util.Arrays;
+
 class ArmstrongNumbers {
 
-    boolean isArmstrongNumber(int numberToCheck) {
+	boolean isArmstrongNumber(int numberToCheck) {
+		int count = 0;
+		int temp = numberToCheck;
 
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+		while (temp != 0) {
+			temp /= 10;
+			count++;
+		}
 
-    }
+		int[] digits = new int[count];
+		temp = numberToCheck;
+		for (int index = count - 1; index >= 0; index--) {
+			digits[index] = temp % 10;
+			temp /= 10;
+		}
+
+		int result = Arrays.stream(digits).map(num -> (int) Math.pow(num, digits.length)).sum();
+		return numberToCheck == result;
+
+	}
 
 }
--- exercism/java/armstrong-numbers/src/test/java/ArmstrongNumbersTest.java
+++ exercism/java/armstrong-numbers/src/test/java/ArmstrongNumbersTest.java
@@ -21,7 +21,6 @@ public class ArmstrongNumbersTest {
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void singleDigitsAreArmstrongNumbers() {
         int input = 5;
@@ -29,7 +28,6 @@ public class ArmstrongNumbersTest {
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void noTwoDigitArmstrongNumbers() {
         int input = 10;
@@ -37,7 +35,6 @@ public class ArmstrongNumbersTest {
         assertFalse(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void threeDigitNumberIsArmstrongNumber() {
         int input = 153;
@@ -45,7 +42,6 @@ public class ArmstrongNumbersTest {
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void threeDigitNumberIsNotArmstrongNumber() {
         int input = 100;
@@ -53,7 +49,6 @@ public class ArmstrongNumbersTest {
         assertFalse(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void fourDigitNumberIsArmstrongNumber() {
         int input = 9474;
@@ -61,7 +56,6 @@ public class ArmstrongNumbersTest {
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void fourDigitNumberIsNotArmstrongNumber() {
         int input = 9475;
@@ -69,7 +63,6 @@ public class ArmstrongNumbersTest {
         assertFalse(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void sevenDigitNumberIsArmstrongNumber() {
         int input = 9926315;
@@ -77,7 +70,6 @@ public class ArmstrongNumbersTest {
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void sevenDigitNumberIsNotArmstrongNumber() {
         int input = 9926314;
