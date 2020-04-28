805af8f761c06fb8be2095006c2ad3337720329d
Saurabh Yadav <ysaurabh@gmail.com>
2020-04-28 20:27:11 +0530

    Grains solution

--- exercism/java/grains/src/main/java/Grains.java
+++ exercism/java/grains/src/main/java/Grains.java
@@ -5,15 +5,11 @@ class Grains {
 	BigInteger grainsOnSquare(final int square) {
 		if (square < 1 || square > 64)
 			throw new IllegalArgumentException("square must be between 1 and 64");
-
-		return BigInteger.valueOf((long) Math.pow(2, square - 1));
+		
+		return BigInteger.valueOf(2).pow(square-1);
 	}
 
 	BigInteger grainsOnBoard() {
-		BigInteger sum = BigInteger.valueOf(0);
-		for (int index = 1; index <= 64; index++) {
-			sum.add(grainsOnSquare(index));
-		}
-		return sum;
+		return BigInteger.valueOf(2).pow(64).subtract(BigInteger.valueOf(1));
 	}
 }
