571177da343ac5bc371da8fa0c3de5c828628abf
Saurabh Yadav <ysaurabh@gmail.com>
2020-04-28 19:55:05 +0530

    Pangram

--- exercism/java/pangram/src/main/java/PangramChecker.java
+++ exercism/java/pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class PangramChecker {
 
-    public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	public boolean isPangram(String input) {
+
+		if ("".equals(input)) {
+			return false;
+		}
+
+		input = input.toUpperCase();
+		Set<Character> charSet = new HashSet<>();
+		long count = input.chars().mapToObj(ch -> (char) ch).filter(Character::isLetter).filter(charSet::add).count();
+
+		return count == 26;
+	}
 
 }
