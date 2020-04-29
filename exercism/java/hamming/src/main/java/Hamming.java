21fff52471e504541f05afa048ccba1e00a955e3
Saurabh Yadav <ysaurabh@gmail.com>
2020-04-29 22:32:10 +0530

    implementaion

--- exercism/java/hamming/src/main/java/Hamming.java
+++ exercism/java/hamming/src/main/java/Hamming.java
@@ -1,9 +1,22 @@
 public class Hamming {
-    public Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
 
-    public int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	private String leftStrand;
+	private String rightStrand;
+
+	public Hamming(String leftStrand, String rightStrand) {
+		if (leftStrand.length() != rightStrand.length()) {
+			throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+		}
+		this.leftStrand = leftStrand;
+		this.rightStrand = rightStrand;
+	}
+
+	public int getHammingDistance() {
+		int distance = 0;
+		for (int index = 0; index < leftStrand.length(); index++) {
+			if (leftStrand.charAt(index) == rightStrand.charAt(index))
+				distance++;
+		}
+		return distance;
+	}
 }
