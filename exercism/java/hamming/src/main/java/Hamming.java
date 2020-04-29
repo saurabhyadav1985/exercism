d36e0c8fb95075cc820245892d7fe360cd4cc79d
Saurabh Yadav <ysaurabh@gmail.com>
2020-04-29 22:44:12 +0530

    minor change in logic hamming

--- exercism/java/hamming/src/main/java/Hamming.java
+++ exercism/java/hamming/src/main/java/Hamming.java
@@ -4,7 +4,13 @@ public class Hamming {
 	private String rightStrand;
 
 	public Hamming(String leftStrand, String rightStrand) {
-		if (leftStrand.length() != rightStrand.length()) {
+		if(leftStrand.isEmpty() && !rightStrand.isEmpty()) {
+			throw new IllegalArgumentException("left strand must not be empty.");
+		}
+		else if(rightStrand.isEmpty() && !leftStrand.isEmpty() ) {
+			throw new IllegalArgumentException("right strand must not be empty.");
+		}
+		else if (leftStrand.length() != rightStrand.length()) {
 			throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
 		}
 		this.leftStrand = leftStrand;
@@ -13,6 +19,11 @@ public class Hamming {
 
 	public int getHammingDistance() {
 		int distance = 0;
+		
+		if("".equals(leftStrand)) {
+			return distance;
+		}
+		
 		for (int index = 0; index < leftStrand.length(); index++) {
 			if (leftStrand.charAt(index) != rightStrand.charAt(index))
 				distance++;
