ea2d8075bcc504f5936d7d998a794bcf54d53805
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-01 11:10:29 +0530

    formating

--- exercism/java/space-age/src/main/java/SpaceAge.java
+++ exercism/java/space-age/src/main/java/SpaceAge.java
@@ -3,7 +3,7 @@ class SpaceAge {
 	double seconds;
 	int earthAge = 31557600;
 	double age = 0;
-	
+
 	SpaceAge(double seconds) {
 		if (seconds < 0) {
 			throw new IllegalArgumentException("Age can not be negative");
@@ -16,11 +16,10 @@ class SpaceAge {
 	}
 
 	double onEarth() {
-		if(age != 0) {
+		if (age != 0) {
 			// Do nothing
-		}
-		else {
-			age = seconds/earthAge;
+		} else {
+			age = seconds / earthAge;
 		}
 		return age;
 	}
@@ -53,15 +52,16 @@ class SpaceAge {
 		return onEarth() / OrbitalPeriod.NEPTUNE.getPeriodFactor();
 	}
 
-	enum OrbitalPeriod {
-		EARTH(1.0),MERCURY(0.2408467),VENUS(0.61519726),MARS(1.8808158),JUPITER(11.862615),SATURN(29.447498),URANUS(84.016846),NEPTUNE(164.79132);
-		
+	private enum OrbitalPeriod {
+		EARTH(1.0), MERCURY(0.2408467), VENUS(0.61519726), MARS(1.8808158), JUPITER(11.862615), SATURN(29.447498),
+		URANUS(84.016846), NEPTUNE(164.79132);
+
 		private double periodFactor;
 
 		private OrbitalPeriod(double periodFactor) {
 			this.periodFactor = periodFactor;
 		}
-		
+
 		public double getPeriodFactor() {
 			return periodFactor;
 		}
