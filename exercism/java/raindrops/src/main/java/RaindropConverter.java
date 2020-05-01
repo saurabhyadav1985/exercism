a30c134c5a8e5342d8e9a2d344dce8d8113c2094
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-01 12:32:07 +0530

    solution of raindrops

--- exercism/java/raindrops/src/main/java/RaindropConverter.java
+++ exercism/java/raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,28 @@
 class RaindropConverter {
+	private final String THREE_FACTOR_OUTPUT = "Pling";
+	private final String FIVE_FACTOR_OUTPUT = "Plang";
+	private final String SEVEN_FACTOR_OUTPUT = "Plong";
 
-    String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	String convert(int number) {
+
+		StringBuilder output = new StringBuilder();
+
+		if (number % 3 != 0 && number % 5 != 0 && number % 7 != 0)
+			return String.valueOf(number);
+		else {
+
+			if (number % 3 == 0) {
+				output.append(THREE_FACTOR_OUTPUT);
+			}
+			if (number % 5 == 0) {
+				output.append(FIVE_FACTOR_OUTPUT);
+			}
+			if (number % 7 == 0) {
+				output.append(SEVEN_FACTOR_OUTPUT);
+			}
+		}
+		
+		return output.toString();
+	}
 
 }
