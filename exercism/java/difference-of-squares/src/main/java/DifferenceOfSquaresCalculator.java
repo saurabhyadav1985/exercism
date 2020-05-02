a56f3561566a2826704fca9adaefaf6026ccf1b9
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-02 12:08:58 +0530

    implementation

--- exercism/java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
+++ exercism/java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,15 +1,17 @@
+import java.util.stream.IntStream;
+
 class DifferenceOfSquaresCalculator {
 
-    int computeSquareOfSumTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	int computeSquareOfSumTo(int input) {
+		int sum = IntStream.rangeClosed(1, input).sum();
+		return sum * sum;
+	}
 
-    int computeSumOfSquaresTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	int computeSumOfSquaresTo(int input) {
+		return IntStream.rangeClosed(1, input).map(index -> index*index).sum();
+	}
 
-    int computeDifferenceOfSquares(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-
+	int computeDifferenceOfSquares(int input) {
+		return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
+	}
 }
