700566bc9fdcc62340b399000a4daff9354d8891
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-04 18:59:11 +0530

    implementation for ascii

--- exercism/java/micro-blog/src/main/java/MicroBlog.java
+++ exercism/java/micro-blog/src/main/java/MicroBlog.java
@@ -1,5 +1,9 @@
 class MicroBlog {
-    public String truncate(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	public String truncate(String input) {
+		if (input.length() <= 5) {
+			return input;
+		} else {
+			return input.substring(0, 5);
+		}
+	}
 }
