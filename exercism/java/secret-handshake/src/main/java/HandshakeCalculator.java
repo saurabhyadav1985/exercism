76d6575fdae68e25f1fda116f72aafca6605fa32
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-05 22:38:08 +0530

    updated logic of hand shake

--- exercism/java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ exercism/java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -7,32 +7,29 @@ class HandshakeCalculator {
 	List<Signal> calculateHandshake(int number) {
 		String binary = Integer.toBinaryString(number);
 		int lengthBinary = binary.length();
-		List<Signal> list = new ArrayList<>();
-		for (int index = 0; index < lengthBinary - 1; index++) {
+		List<Signal> signals = new ArrayList<>();
+		for (int index = lengthBinary - 1; index >= 0; index--) {
 			if (binary.charAt(index) == '1') {
 				int value = 1 * (int) Math.pow(10, lengthBinary - 1 - index);
 				switch (value) {
 				case 1:
-					list.add(Signal.WINK);
+					signals.add(Signal.WINK);
 					break;
 				case 10:
-					list.add(Signal.DOUBLE_BLINK);
+					signals.add(Signal.DOUBLE_BLINK);
 					break;
 				case 100:
-					list.add(Signal.CLOSE_YOUR_EYES);
+					signals.add(Signal.CLOSE_YOUR_EYES);
 					break;
 				case 1000:
-					list.add(Signal.JUMP);
+					signals.add(Signal.JUMP);
 					break;
 				case 10000:
-					break;
-				default:
+					Collections.reverse(signals);
 					break;
 				}
 			}
 		}
-
-		return list;
+		return signals;
 	}
-
 }
