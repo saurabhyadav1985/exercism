d8461bc30a9503b0da9babc813c8a4eb4cda91bf
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-05 22:10:21 +0530

    first commit for handshake calculator

--- exercism/java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ exercism/java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,9 +1,23 @@
+import java.util.Collections;
 import java.util.List;
 
 class HandshakeCalculator {
 
-    List<Signal> calculateHandshake(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	List<Signal> calculateHandshake(int number) {
+		String binary = Integer.toBinaryString(number);
+
+		int index = 1;
+		while (index < binary.length()) {
+			int firstPosition = 0;
+			while (binary.charAt(index) == '0') {
+				index++;
+			}
+
+			String str = binary.substring(firstPosition, index);
+			System.out.println(str);
+			firstPosition =index;
+		}
+		return Collections.EMPTY_LIST;
+	}
 
 }
