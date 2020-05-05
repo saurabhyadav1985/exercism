c33a18ccfc1a508197940ffe0a8dd3b0768db23c
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-05 22:29:44 +0530

    Updates

--- exercism/java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ exercism/java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -5,19 +6,33 @@ class HandshakeCalculator {
 
 	List<Signal> calculateHandshake(int number) {
 		String binary = Integer.toBinaryString(number);
-
-		int index = 1;
-		while (index < binary.length()) {
-			int firstPosition = 0;
-			while (binary.charAt(index) == '0') {
-				index++;
+		int lengthBinary = binary.length();
+		List<Signal> list = new ArrayList<>();
+		for (int index = 0; index < lengthBinary - 1; index++) {
+			if (binary.charAt(index) == '1') {
+				int value = 1 * (int) Math.pow(10, lengthBinary - 1 - index);
+				switch (value) {
+				case 1:
+					list.add(Signal.WINK);
+					break;
+				case 10:
+					list.add(Signal.DOUBLE_BLINK);
+					break;
+				case 100:
+					list.add(Signal.CLOSE_YOUR_EYES);
+					break;
+				case 1000:
+					list.add(Signal.JUMP);
+					break;
+				case 10000:
+					break;
+				default:
+					break;
+				}
 			}
-
-			String str = binary.substring(firstPosition, index);
-			System.out.println(str);
-			firstPosition =index;
 		}
-		return Collections.EMPTY_LIST;
+
+		return list;
 	}
 
 }
