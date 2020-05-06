3523cddfeab76ebf64611a788d8e23152d9accf3
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-06 23:17:46 +0530

    impl constructors of exceptions

--- exercism/java/error-handling/src/main/java/CustomUncheckedException.java
+++ exercism/java/error-handling/src/main/java/CustomUncheckedException.java
@@ -1,11 +1,11 @@
 class CustomUncheckedException extends RuntimeException {
 
-    CustomUncheckedException() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	CustomUncheckedException() {
+		super();
+	}
 
-    CustomUncheckedException(String message) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	CustomUncheckedException(String message) {
+		super(message);
+	}
 
 }
