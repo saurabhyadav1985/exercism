8bb0419a6c4377f8e3da088ddc0f544177cd2246
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-07 22:35:30 +0530

    added impl for all methods

--- exercism/java/error-handling/src/main/java/ErrorHandling.java
+++ exercism/java/error-handling/src/main/java/ErrorHandling.java
@@ -2,48 +2,53 @@ import java.util.Optional;
 
 class ErrorHandling {
 
-    void handleErrorByThrowingIllegalArgumentException() {
-        throw new IllegalArgumentException("Invalid input");
-    }
+	void handleErrorByThrowingIllegalArgumentException() {
+		throw new IllegalArgumentException("Invalid input");
+	}
 
-    void handleErrorByThrowingIllegalArgumentExceptionWithDetailMessage(String message) {
-        throw new IllegalArgumentException(message);
-    }
+	void handleErrorByThrowingIllegalArgumentExceptionWithDetailMessage(String message) {
+		throw new IllegalArgumentException(message);
+	}
 
-    void handleErrorByThrowingAnyCheckedException() throws CustomCheckedException{
-        throw new CustomCheckedException();
-    }
+	void handleErrorByThrowingAnyCheckedException() throws CustomCheckedException {
+		throw new CustomCheckedException();
+	}
 
-    void handleErrorByThrowingAnyCheckedExceptionWithDetailMessage(String message) throws CustomCheckedException {
-    	throw new CustomCheckedException(message);
-    }
+	void handleErrorByThrowingAnyCheckedExceptionWithDetailMessage(String message) throws CustomCheckedException {
+		throw new CustomCheckedException(message);
+	}
 
-    void handleErrorByThrowingAnyUncheckedException() {
-        throw new CustomUncheckedException();
-    }
+	void handleErrorByThrowingAnyUncheckedException() {
+		throw new CustomUncheckedException();
+	}
 
-    void handleErrorByThrowingAnyUncheckedExceptionWithDetailMessage(String message) {
-    	throw new CustomUncheckedException(message);
-    }
+	void handleErrorByThrowingAnyUncheckedExceptionWithDetailMessage(String message) {
+		throw new CustomUncheckedException(message);
+	}
 
-    void handleErrorByThrowingCustomCheckedException() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	void handleErrorByThrowingCustomCheckedException() throws CustomCheckedException {
+		throw new CustomCheckedException();
+	}
 
-    void handleErrorByThrowingCustomCheckedExceptionWithDetailMessage(String message) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	void handleErrorByThrowingCustomCheckedExceptionWithDetailMessage(String message) throws CustomCheckedException {
+		throw new CustomCheckedException(message);
+	}
 
-    void handleErrorByThrowingCustomUncheckedException() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	void handleErrorByThrowingCustomUncheckedException() {
+		throw new CustomUncheckedException();
+	}
 
-    void handleErrorByThrowingCustomUncheckedExceptionWithDetailMessage(String message) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	void handleErrorByThrowingCustomUncheckedExceptionWithDetailMessage(String message) {
+		throw new CustomUncheckedException(message);
+	}
 
-    Optional<Integer> handleErrorByReturningOptionalInstance(String integer) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	Optional<Integer> handleErrorByReturningOptionalInstance(String integer) {
+		try {
+			return Optional.of(Integer.parseInt(integer));
+		} catch (Exception e) {
+			return Optional.empty();
+		}
+
+	}
 
 }
