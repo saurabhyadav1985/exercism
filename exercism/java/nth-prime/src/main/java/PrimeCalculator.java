c1c5ea4723d62e8b9962f29b41d750c313c5283c
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-08 22:39:19 +0530

    added implementation

--- exercism/java/nth-prime/src/main/java/PrimeCalculator.java
+++ exercism/java/nth-prime/src/main/java/PrimeCalculator.java
@@ -1,7 +1,26 @@
 class PrimeCalculator {
 
-    int nth(int nth) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	int nth(int nth) {
+		int position = 0;
+		int number = 2;
+		while (position <= nth) {
+			if (isPrime(number)) {
+				System.out.println(number);
+				position++;
+				if(position == nth) break;
+			}
+			number++;
+		}
+		return number;
+	}
+
+	boolean isPrime(int number) {
+		for (int index = 2; index <= number / 2; index++) {
+			if (number % index == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
