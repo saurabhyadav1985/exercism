1cf152c47bf542ed98403d250074061591c47a37
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-11 22:21:40 +0530

    added empty array check

--- exercism/java/proverb/src/main/java/Proverb.java
+++ exercism/java/proverb/src/main/java/Proverb.java
@@ -7,6 +7,9 @@ class Proverb {
 	}
 
 	String recite() {
+		if (words.length == 0) {
+			return "";
+		}
 		StringBuilder reciteStrBuilder = new StringBuilder();
 		for (int index = 0; index < words.length - 1; index++) {
 			reciteStrBuilder.append("For want of a " + words[index] + " the " + words[index + 1] + " was lost.\n");
