0c3cb155b56280d2117c6fc40813bf29efb0a1f3
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-11 22:20:16 +0530

    added implementation

--- exercism/java/proverb/src/main/java/Proverb.java
+++ exercism/java/proverb/src/main/java/Proverb.java
@@ -1,11 +1,18 @@
 class Proverb {
 
-    Proverb(String[] words) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	String[] words;
 
-    String recite() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	Proverb(String[] words) {
+		this.words = words;
+	}
+
+	String recite() {
+		StringBuilder reciteStrBuilder = new StringBuilder();
+		for (int index = 0; index < words.length - 1; index++) {
+			reciteStrBuilder.append("For want of a " + words[index] + " the " + words[index + 1] + " was lost.\n");
+		}
+		reciteStrBuilder.append("And all for the want of a " + words[0] + ".");
+		return reciteStrBuilder.toString();
+	}
 
 }
