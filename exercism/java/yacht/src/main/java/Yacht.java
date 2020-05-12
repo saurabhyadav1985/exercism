96bb04fb8965d027f321e3404ad98e377a75ed93
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-12 19:30:51 +0530

    added impl

--- exercism/java/yacht/src/main/java/Yacht.java
+++ exercism/java/yacht/src/main/java/Yacht.java
@@ -1,11 +1,62 @@
+import java.util.Arrays;
+
 class Yacht {
+	int[] dice;
+	YachtCategory category;
 
-    Yacht(int[] dice, YachtCategory yachtCategory) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	Yacht(int[] dice, YachtCategory yachtCategory) {
+		this.dice = dice;
+		this.category = yachtCategory;
+	}
 
-    int score() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	int score() {
+		int score = 0;
+		switch (category.ordinal()) {
 
+		case 0:
+			score = 50;
+			break;
+		case 1:
+			score = (int) Arrays.stream(dice).filter(i -> i == 1).count();
+			break;
+		case 2:
+			score = 2 * (int) Arrays.stream(dice).filter(i -> i == 2).count();
+			;
+			break;
+		case 3:
+			score = 3 * (int) Arrays.stream(dice).filter(i -> i == 3).count();
+			;
+			break;
+		case 4:
+			score = 4 * (int) Arrays.stream(dice).filter(i -> i == 4).count();
+			;
+			break;
+		case 5:
+			score = 5 * (int) Arrays.stream(dice).filter(i -> i == 5).count();
+			;
+			break;
+		case 6:
+			score = 6 * (int) Arrays.stream(dice).filter(i -> i == 6).count();
+			;
+			break;
+		case 7:
+			score = (int) Arrays.stream(dice).sum();
+			;
+			break;
+		case 8:
+			score = (int) Arrays.stream(dice).filter(i -> i == 4).count();
+			;
+			break;
+		case 9:
+			score = (int) Arrays.stream(dice).sum();
+			break;
+		case 10:
+			score = (int) Arrays.stream(dice).sum();
+			break;
+		case 11:
+			score = (int) Arrays.stream(dice).sum();
+			break;
+		}
+		return score;
+	}
 }
