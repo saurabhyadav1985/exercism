e1d814beb6028214606d05aaf1ea26ba90be8b1e
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-13 21:42:07 +0530

    Added fix for X character at the end

--- exercism/java/isbn-verifier/src/main/java/IsbnVerifier.java
+++ exercism/java/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -6,12 +6,21 @@ class IsbnVerifier {
 		}
 		int multiplier = 10;
 		int isbn = 0;
+		int count = 0;
 		for (char ch : stringToVerify.toCharArray()) {
 			if (ch != '-') {
-				isbn = isbn + Integer.valueOf(ch) * multiplier--;
+				count++;
+				if (ch == 'X') {
+					if( count != 10) {
+						return false;
+					}
+					isbn = isbn + 10 * multiplier--;
+				} else {
+					isbn = isbn + (Integer.valueOf(ch)-48) * multiplier--;
+				}
 			}
 		}
-		if (isbn % 11 == 0) {
+		if (isbn % 11 == 0 && count ==10) {
 			return true;
 		} else {
 			return false;
