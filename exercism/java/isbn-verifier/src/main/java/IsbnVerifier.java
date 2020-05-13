3e45efa0b76dd2d17b1cafe180c43e8b96658165
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-13 21:28:31 +0530

    Added fix for empty isnb

--- exercism/java/isbn-verifier/src/main/java/IsbnVerifier.java
+++ exercism/java/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,19 +1,21 @@
 class IsbnVerifier {
 
-    boolean isValid(String stringToVerify) {
-        int multiplier = 10;
-        int isbn =0;
-        for(char ch: stringToVerify.toCharArray()) {
-        	if(ch != '-') {
-        		isbn = isbn + Integer.valueOf(ch)*multiplier--;
-        	}
-        }
-        if(isbn % 11 == 0) {
-        	return true;
-        }
-        else {
-        	return false;
-        }
-    }
+	boolean isValid(String stringToVerify) {
+		if ("".equals(stringToVerify)) {
+			return false;
+		}
+		int multiplier = 10;
+		int isbn = 0;
+		for (char ch : stringToVerify.toCharArray()) {
+			if (ch != '-') {
+				isbn = isbn + Integer.valueOf(ch) * multiplier--;
+			}
+		}
+		if (isbn % 11 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 }
