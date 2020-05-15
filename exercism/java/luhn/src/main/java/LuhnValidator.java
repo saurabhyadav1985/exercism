631dd1d48809a430ff0fba331dc8c8fd218ca7f1
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-15 19:53:19 +0530

    updated implementation

--- exercism/java/luhn/src/main/java/LuhnValidator.java
+++ exercism/java/luhn/src/main/java/LuhnValidator.java
@@ -3,7 +3,11 @@ class LuhnValidator {
 	boolean isValid(String candidate) {
 		int sum = 0;
 		for (int index = candidate.length() - 1; index >= 0; index = index - 1) {
+
 			int value = candidate.charAt(index) - 48;
+			if (value < 0 && value > 9) {
+				return false;
+			}
 			if (index % 2 == 0) {
 				value = value * 2;
 				if (value > 9) {
@@ -12,7 +16,7 @@ class LuhnValidator {
 			}
 			sum += value;
 		}
-		return sum%10 ==0;
+		return sum % 10 == 0;
 	}
 
 }
