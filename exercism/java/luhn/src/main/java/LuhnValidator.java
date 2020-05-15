01fa154dbcf59a478d37795629d36cbb93bfa831
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-15 19:59:24 +0530

    fixed even odd issue

--- exercism/java/luhn/src/main/java/LuhnValidator.java
+++ exercism/java/luhn/src/main/java/LuhnValidator.java
@@ -3,13 +3,14 @@ class LuhnValidator {
 	boolean isValid(String candidate) {
 		candidate = candidate.replaceAll(" ", "");
 		int sum = 0;
+		boolean isEven = true;
 		for (int index = candidate.length() - 1; index >= 0; index = index - 1) {
-
+			isEven = !isEven;
 			int value = candidate.charAt(index) - 48;
 			if (value < 0 && value > 9) {
 				return false;
 			}
-			if (index % 2 == 0) {
+			if (isEven) {
 				value = value * 2;
 				if (value > 9) {
 					value = value - 9;
