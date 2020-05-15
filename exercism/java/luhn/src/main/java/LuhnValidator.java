5c3b2d6129c33cd970ec6132c07347fee35cd7e6
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-15 19:50:53 +0530

    added implementation

--- exercism/java/luhn/src/main/java/LuhnValidator.java
+++ exercism/java/luhn/src/main/java/LuhnValidator.java
@@ -1,7 +1,18 @@
 class LuhnValidator {
 
-    boolean isValid(String candidate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+	boolean isValid(String candidate) {
+		int sum = 0;
+		for (int index = candidate.length() - 1; index >= 0; index = index - 1) {
+			int value = candidate.charAt(index) - 48;
+			if (index % 2 == 0) {
+				value = value * 2;
+				if (value > 9) {
+					value = value - 9;
+				}
+			}
+			sum += value;
+		}
+		return sum%10 ==0;
+	}
 
 }
