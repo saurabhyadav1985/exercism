eb434d192964916a2858686b1b5c87252b939b10
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-16 12:30:56 +0530

    updated implementation

--- exercism/java/luhn/src/main/java/LuhnValidator.java
+++ exercism/java/luhn/src/main/java/LuhnValidator.java
@@ -1,28 +1,31 @@
 class LuhnValidator {
 
 	boolean isValid(String candidate) {
-		candidate = candidate.replaceAll(" ", "");
-		if("0".equals(candidate.trim())) {
+		if ("0".equals(candidate.trim())) {
 			return false;
 		}
 		int sum = 0;
 		boolean isEven = true;
 		for (int index = candidate.length() - 1; index >= 0; index = index - 1) {
-			isEven = !isEven;
-			if(!Character.isDigit(candidate.charAt(index))){
-				return false;
-			}
-			int value = candidate.charAt(index) - 48;
-			if (value < 0 && value > 9) {
-				return false;
-			}
-			if (isEven) {
-				value = value * 2;
-				if (value > 9) {
-					value = value - 9;
+
+			char ch = candidate.charAt(index);
+			if (ch != 32) {
+				isEven = !isEven;
+				if (!Character.isDigit(ch)) {
+					return false;
 				}
+				int value = ch - 48;
+				if (value < 0 && value > 9) {
+					return false;
+				}
+				if (isEven) {
+					value = value * 2;
+					if (value > 9) {
+						value = value - 9;
+					}
+				}
+				sum += value;
 			}
-			sum += value;
 		}
 		return sum % 10 == 0;
 	}
