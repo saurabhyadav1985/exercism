e8d0814fc6b66ecce78067794b9826dafc2b68e3
Saurabh Yadav <ysaurabh@gmail.com>
2020-05-16 12:34:32 +0530

    tried to reduce complexity

--- exercism/java/luhn/src/main/java/LuhnValidator.java
+++ exercism/java/luhn/src/main/java/LuhnValidator.java
@@ -4,20 +4,19 @@ class LuhnValidator {
 		if ("0".equals(candidate.trim())) {
 			return false;
 		}
+
 		int sum = 0;
 		boolean isEven = true;
 		for (int index = candidate.length() - 1; index >= 0; index = index - 1) {
 
 			char ch = candidate.charAt(index);
 			if (ch != 32) {
-				isEven = !isEven;
 				if (!Character.isDigit(ch)) {
 					return false;
 				}
+
+				isEven = !isEven;
 				int value = ch - 48;
-				if (value < 0 && value > 9) {
-					return false;
-				}
 				if (isEven) {
 					value = value * 2;
 					if (value > 9) {
